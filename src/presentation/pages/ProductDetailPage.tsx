78e0ee3506714d9022af2ee22ba2a43eb1bd9186
Nahom Habtamu <nahomhab2626@gmail.com>
2022-11-08 11:16:08 +0300

    fix: delete ui from last commit on product detail page    - remove unecessary dependencies

--- src/presentation/pages/ProductDetailPage.tsx
+++ src/presentation/pages/ProductDetailPage.tsx
@@ -1,21 +1,5 @@
-import { useEffect } from "react";
-import ProductDetailCarousel from "../components/product_detail/ProductDetailCarousel";
-import {
-  ProductDetailTitleBadgeStyled,
-  ProductDetailTitleWrapperStyled,
-  ProductDetailWrapperStyled,
-} from "../styled_components/product_detail/ProductDetailComponentsStyled";
-
 const ProductDetailPage = () => {
-  return (
-    <ProductDetailWrapperStyled>
-      <ProductDetailTitleWrapperStyled>
-        <ProductDetailTitleBadgeStyled>Title</ProductDetailTitleBadgeStyled>
-        cycle count 11, macbook pro 2019"6 laptop, 512gb ssd
-      </ProductDetailTitleWrapperStyled>
-      <ProductDetailCarousel></ProductDetailCarousel>
-    </ProductDetailWrapperStyled>
-  );
+  return <>I AM WORKING ON DETAIL PAGE</>;
 };
 
 export default ProductDetailPage;
