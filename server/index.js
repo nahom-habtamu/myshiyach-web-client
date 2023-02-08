25a21b061c76391a15be288e4d9671ecc03c421d
Nahom Habtamu <nahomhab2626@gmail.com>
2023-02-08 10:34:40 +0300

    patch: change prod port to 8080

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const publicPath = path.join(__dirname, '..', 'build');
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(publicPath));
 
