2fab2d7cf4b811a757e90c4748476432ca8e6dbe
Nahom Habtamu <nahomhab2626@gmail.com>
2023-03-06 15:17:28 +0300

    patch: change ip of api to support https

--- src/core/utils/api.ts
+++ src/core/utils/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://167.172.148.80:5000/api",
+  // baseURL: "http://167.172.148.80:5000/api",
+  baseURL: "https://134.209.130.128:5000/api",
 });
 
 export default instance;
