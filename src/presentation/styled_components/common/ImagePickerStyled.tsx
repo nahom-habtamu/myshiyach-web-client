e7271c560b23cc88ed700a976aff22e3fb7b9275
Nahom Habtamu <nahomhab2626@gmail.com>
2023-04-19 12:17:37 +0300

    refactor: remove unused imports

--- src/presentation/styled_components/common/ImagePickerStyled.tsx
+++ src/presentation/styled_components/common/ImagePickerStyled.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { DARK_PINK_COLOR, PINK_COLOR, PRIMARY_COLOR, PRIMARY_LIGHT_COLOR } from "../../constants/colors";
+import { PINK_COLOR } from "../../constants/colors";
 
 const ImagePickerWrapperStyled = styled.div`
   width: 92%;
